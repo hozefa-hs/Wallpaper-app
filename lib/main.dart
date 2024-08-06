154cfb51011e5c6b0e938dbf6563458b73ec0858
Hozefa <hozefasailanawala786@gmail.com>
2024-08-06 20:52:50 +0530

    Fixed status bar visibility

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:wallpaper_app/screens/splash_screen.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        fontFamily: "Poppins"
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          fontFamily: "Poppins"),
       home: const SplashScreen(),
     );
   }
